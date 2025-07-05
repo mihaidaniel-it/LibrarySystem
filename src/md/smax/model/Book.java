9048865cd956f8718233faf98f3eae5068526e47
Daniel <danielmihai.it@mail.ru>
2025-07-05 20:07:09 +0300

    Added getters, setter and method which get all information about book

--- src/md/smax/model/Book.java
+++ src/md/smax/model/Book.java
@@ -1,4 +1,48 @@
 package md.smax.model;
 
 public class Book {
+//    Fields
+    private String title;
+    private String author;
+    private int year;
+    private String genre;
+
+//    Setters
+    void setTitle(String title) {
+        this.title = title;
+    }
+
+    void setAuthor(String author) {
+        this.author = author;
+    }
+
+    void setYear(int year) {
+        this.year = year;
+    }
+
+    void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+//    Getters
+    String getTitle() {
+        return this.title;
+    }
+
+    String getAuthor() {
+        return this.author;
+    }
+
+    int getYear() {
+        return this.year;
+    }
+
+    String getGenre() {
+        return this.genre;
+    }
+
+//    Methods
+    public String getBookInfo() {
+        System.out.println("Book " + this.title + " written by " + this.author + " in " + this.year + ". Genre: " + this.genre);
+    }
 }
