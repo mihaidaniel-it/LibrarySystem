3693f64c91e3ec5b02f1601ccec6e39362a168fc
Daniel <danielmihai.it@mail.ru>
2025-07-07 09:59:43 +0300

    Added functionality bassed of classes Book and LibraryServices & edited LibraryServices

--- src/md/smax/Main.java
+++ src/md/smax/Main.java
@@ -4,9 +4,41 @@ import md.smax.services.LibraryService;
 
 public class Main {
     public static void main (String[] args) {
-// todo: Создай объект LibraryService
-// todo:Добавь в него 2–3 книги
-// todo: Выведи список всех книг
-// todo: Проведи поиск по названию и автору
+//        Create objects
+        LibraryService libraryService = new LibraryService();
+
+        Book book1 = new Book();
+        Book book2 = new Book();
+        Book book3 = new Book();
+
+//        Data assignment
+        book1.setTitle("Can't Hurt Me");
+        book1.setAuthor("David Goggins");
+        book1.setGenre("Motivation");
+        book1.setYear(2018);
+
+        book2.setTitle("Think and get Rich");
+        book2.setAuthor("Napoleon Hill");
+        book2.setGenre("Psychology");
+        book2.setYear(1937);
+
+        book3.setTitle("Rich dad Poor dad");
+        book3.setAuthor("Robert Kiyosaki");
+        book3.setGenre("Finance");
+        book3.setYear(1997);
+
+        libraryService.addBook(book1);
+        libraryService.addBook(book2);
+        libraryService.addBook(book3);
+
+//        Output
+        libraryService.listBooks();
+        libraryService.searchByAuthor("David Goggins");
+        libraryService.searchByTitle("Rich dad Poor dad");
+        libraryService.searchByGenre("Psychology");
+
+        libraryService.searchByAuthor("Daniel Mihai");
+        libraryService.searchByTitle("Deep Work");
+        libraryService.searchByGenre("Love");
     }
 }
--- src/md/smax/services/LibraryService.java
+++ src/md/smax/services/LibraryService.java
@@ -28,9 +28,10 @@ public class LibraryService {
             if (books[i].getTitle().equals(title)) {
                 System.out.println(books[i].getBookInfo());
                 bookFound = true;
-            } else if (!bookFound) {
-                System.out.println("We don't have book - " + "'" + title + "'");
             }
+        }
+        if (!bookFound) {
+            System.out.println("We don't have book - " + "'" + title + "'");
         }
     }
 
@@ -39,12 +40,24 @@ public class LibraryService {
         for (int i = 0; i < bookCount; i++) {
             if (books[i].getAuthor().equals(author)) {
                 System.out.println(books[i].getBookInfo());
-            } else if (!authorFound) {
-                System.out.println("We don't have books of " + author);
+                authorFound = true;
             }
+        }
+        if (!authorFound) {
+            System.out.println("We don't have books of " + author);
+        }
+    }
+
+    public void searchByGenre(String genre) {
+        boolean genreFound = false;
+        for (int i = 0; i < bookCount; i++) {
+            if (books[i].getGenre().equals(genre)) {
+                System.out.println(books[i].getBookInfo());
+                genreFound = true;
+            }
+        }
+        if (!genreFound) {
+            System.out.println("We don't have books of " + genre + " genre");
         }
     }
 }
-
-
-
